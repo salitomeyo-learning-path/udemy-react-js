8d70eb18fbdf2d71beb92d00dd2a465fc5e897dd
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-05 13:24:13 -0500

    feat: jest expect, toBe

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,3 +1,10 @@
-test('Esta prueba no debe falla', () => {
+describe('Pruebas en <DemoComponent />', () => {
+    test('Esta prueba no debe falla', () => {
+        const message1 = 'Hola mundo';
     
+        const message2 = message1.trim();
+    
+        expect( message1 ).toBe( message2 );
+    })
 })
+
