de3fc650427591efba1fae9e932d75242afbf9a9
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-05 10:13:40 -0500

    feat: fragmento

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,9 @@
+export const FirstApp = () => {
+    return (
+        <>
+            <h1>Salome</h1>
+            <p>I'm learning react</p>
+        </>
+    )
+}
+
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FirstApp } from "./FirstApp";
 
-function App() {
-    return <h1>Hola mundo</h1>;
-}
+import { HelloWorldApp } from './HelloWorldApp';
+
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App/>
+        <FirstApp/>
     </React.StrictMode>
 )
