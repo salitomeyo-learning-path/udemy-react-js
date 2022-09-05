648159b6297c4d06c15874f838eaaa1f60ae71ba
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-05 17:47:20 -0500

    feat: add item to list, using map to create html tags

---
+++ 04-gif-expert-app/src/pages/GifExpertApp.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        setCategories( [...categories, 'PEpe'] )
+    }
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <button onClick={ onAddCategory }>Agregar</button>
+            <ol>
+                { categories.map( category => {
+                    return <li key={ category }>{category}</li>
+                }) }
+            </ol>
+        </>
+    )
+}
+
