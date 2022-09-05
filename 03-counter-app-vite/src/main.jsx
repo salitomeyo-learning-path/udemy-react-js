d5a398787164b22ad4b7d5e962e7c25fdd8b996f
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-05 10:26:55 -0500

    feat: importing styles

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -4,6 +4,7 @@ import { FirstApp } from "./FirstApp";
 
 import { HelloWorldApp } from './HelloWorldApp';
 
+import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
