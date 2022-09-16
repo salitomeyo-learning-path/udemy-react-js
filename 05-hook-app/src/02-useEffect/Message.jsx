19b123dcaa5ac83c0a2cc1e432dcb150e53d88c1
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-16 10:18:02 -0500

    feat: useEffect precautions

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -2,10 +2,16 @@ import { useEffect } from "react"
 
 export const Message = () => {
     useEffect(() => {
-        console.log('Message Mounted');
+
+        const onMouseMove = ({ x, y }) => {
+            const coords = { x, y };
+            console.log(coords);
+        }
+        
+        window.addEventListener( 'mousemove', onMouseMove);
 
         return () => {
-            console.log('Message unMounted');
+            window.removeEventListener( 'mousemove', onMouseMove );
         }
     }, [])
 
