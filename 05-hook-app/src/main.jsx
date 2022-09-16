8602b587f0d17e0329d34123bec90f130d198828
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-16 11:46:52 -0500

    feat: useRef

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>
 )
