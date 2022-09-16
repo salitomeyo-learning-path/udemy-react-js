37a1f4237390c52de3c5e7792d5d89bebe92acc9
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-16 10:00:52 -0500

    feat: simple form

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
-import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { HooksApp } from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
-    <CounterWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>
 )
