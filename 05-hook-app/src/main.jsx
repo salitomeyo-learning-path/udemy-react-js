bf7bb71cef629c0adfb1d0d3f23aaccdd5dd4403
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-16 14:13:13 -0500

    feat: react memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <Memorize />
   </React.StrictMode>
 )
